f2bdaac4018586c25779dd1e40849ac1946e5ef5
Matheus <mathszz.2008@gmail.com>
2026-06-12 17:31:00 -0300

    first commit

---
+++ Projeto_backend/Projeto_backend/src/routes/voos.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:from/:to", (req, res) => {
+    const { from, to } = req.params;
+
+    res.json({
+        flight: {
+            from: from.toUpperCase(),
+            to: to.toUpperCase(),
+            price: 2000
+        }
+    });
+});
+
+export default router;
---
+++ Projeto_backend/Projeto_backend/src/server.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'node:http';
+
+const server =  createServer((req, res) =>{
+    res.end("Olá Mundo")
+})
+
+server.listen(3000, () => {
+    console.log('Servidor funcionando em http://localhost:3000');  
+})
